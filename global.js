d6b4787fa5a445a8aaba1ffbf709af1c83b56dbc
raylu15742cs <raylucs15742@gmail.com>
2022-11-14 13:52:01 -0800

    update people

--- global.js
+++ global.js
@@ -2,14 +2,14 @@
 
 //console.log(global);
 
-setTimeout(() => {
-    console.log("in the timeout");
-    clearInterval(int);
-}, 3000);
+//setTimeout(() => {
+//    console.log("in the timeout");
+//    clearInterval(int);
+//}, 3000);
 
-const int = setInterval(() => {
-    console.log("in the interval");
-}, 1000);
+//const int = setInterval(() => {
+//    console.log("in the interval");
+//}, 1000);
 
-console.log(__dirname);
-console.log(__filename);
+//console.log(__dirname);
+//console.log(__filename);
