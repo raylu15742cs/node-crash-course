7a55c045b8fb66b9f653f88a9fd240153b7fbf89
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 15:49:29 -0800

    update views

--- lesson3/server.js
+++ lesson3/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer(( req , res ) => {
     console.log(req.url, req.method);
@@ -6,7 +7,15 @@ const server = http.createServer(( req , res ) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(3000, 'localhost' , () => {
