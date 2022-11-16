254881b608831e068ee75d8f8cf566c8456da84b
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 08:10:33 -0800

    update files

--- files.js
+++ files.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
 // reading files
-fs.readFile("./docs/blog1.txt", (err, data) => {
-    if (err) {
-       console.log(err)
-    }
-    console.log(data.toString());
-});
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//     if (err) {
+//        console.log(err)
+//     }
+//     console.log(data.toString());
+// });
 
-console.log("last line")
+// console.log("last line")
 
 // writing files
 
