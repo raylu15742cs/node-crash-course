15cd94fe40b99d63af884d5a0655f7c24b75cb98
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 15:41:35 -0800

    update server

--- lesson3/server.js
+++ lesson3/server.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 const server = http.createServer(( req , res ) => {
     console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('hello, ninjas');
+    res.end();
 });
 
 server.listen(3000, 'localhost' , () => {
