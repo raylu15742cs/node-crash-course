bac451659d044c284bc3ede9201cac30ddcfdbb4
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 08:41:15 -0800

    piping

--- streams.js
+++ streams.js
@@ -3,9 +3,13 @@ const fs = require('fs')
 const readStreams = fs.createReadStream('./docs/blog3.txt', {encoding : 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-readStreams.on('data' , (chunk) => {
-    console.log("---- NEW CHUNK -----");
-    console.log(chunk);
-    writeStream.write('\n NEW CHUNK \n')
-    writeStream.write(chunk)
-})
+// readStreams.on('data' , (chunk) => {
+//     console.log("---- NEW CHUNK -----");
+//     console.log(chunk);
+//     writeStream.write('\n NEW CHUNK \n')
+//     writeStream.write(chunk)
+// })
+
+// piping
+
+readStreams.pipe(writeStream);
