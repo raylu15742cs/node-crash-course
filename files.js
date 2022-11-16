e7e4800b8bbd9177333622d0106be993cb8658f6
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 08:14:18 -0800

    update files and blog

--- files.js
+++ files.js
@@ -15,6 +15,11 @@ const fs = require('fs');
 fs.writeFile('./docs/blog1.txt' , 'hello,world', () => {
     console.log('file was written');
 });
+
+fs.writeFile('./docs/blog2.txt', 'hello,again', () => {
+  console.log('file was written');
+});
+
 // directories
 
 // deleting files
