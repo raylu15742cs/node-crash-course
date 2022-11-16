07825e739f0d4f9486af841d7b61288ccd862bf5
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 08:39:51 -0800

    write stream

--- streams.js
+++ streams.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 
 const readStreams = fs.createReadStream('./docs/blog3.txt', {encoding : 'utf8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 readStreams.on('data' , (chunk) => {
     console.log("---- NEW CHUNK -----");
     console.log(chunk);
+    writeStream.write('\n NEW CHUNK \n')
+    writeStream.write(chunk)
 })
