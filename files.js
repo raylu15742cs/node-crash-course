56cadd3d396216c0a8143ad7ec2a5e552311abb3
raylu15742cs <raylucs15742@gmail.com>
2022-11-16 08:21:54 -0800

    remvoe files

--- files.js
+++ files.js
@@ -28,6 +28,22 @@ if (!fs.existsSync('./assets')) {
         }
         console.log('folder created')
     })
+} else {
+    fs.rmdir('./assets', (err) => {
+        if(err ) {
+            console.log(err)
+        }
+        console.log('folder deleted');
+    })
 }
 
 // deleting files
+
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted')
+    } )
+}
