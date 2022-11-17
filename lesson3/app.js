e399b3f76f4d3d3e6a6b4aae05c4681910e0829f
raylu15742cs <raylucs15742@gmail.com>
2022-11-17 12:31:36 -0800

    simple nav bar

--- lesson3/app.js
+++ lesson3/app.js
@@ -9,6 +9,10 @@ app.listen(3000);
 
 app.get('/' , (req , res) => {
 
-    res.send('<p>home page </p>');
+    res.sendFile('./views/index.html', {root: __dirname});
 
 })
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', { root: __dirname });
+});
